0077829bc8daab12a2b1e152173fe5ac5d8b9571
muliro <mulirokhaemba@gmail.com>
2022-09-29 21:37:23 +0300

    update and destroy lab

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,3 +1,7 @@
+
+
+
+
 class PlantsController < ApplicationController
 
   # GET /plants
@@ -18,9 +22,26 @@ class PlantsController < ApplicationController
     render json: plant, status: :created
   end
 
+  #UPDATE /plants/:id
+  def update
+    plant = find_plant
+    plant.update(plant_params)
+    render json: plant
+  end
+
+  #DELETE /plants/:id
+  def destroy
+    plant = find_plant
+    plant.destroy
+    head :no_content
+  end
   private
 
   def plant_params
     params.permit(:name, :image, :price, :is_in_stock)
+  end
+
+  def find_plant
+    Plant.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
+
+
+
+
+
+
+
+
 Rails.application.routes.draw do
-  resources :plants, only: [:index, :show, :create]
+  resources :plants
 end
